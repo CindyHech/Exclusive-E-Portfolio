3096312751d19630781a2cf0c538c2b35906072d
Cindy Hech <archerychinadiva@gmail.com>
2025-10-08 23:50:14 -0400

    java email template successfully integrated

---
+++ index.js
@@ -0,0 +1,36 @@
+// template_j4y6w6p
+// service_gizjigr
+// EXBJtKBHRblraxYsv
+
+// //<script type="text/javascript"
+//         src="https://cdn.jsdelivr.net/npm/@emailjs/browser@4/dist/email.min.js">
+// </script>
+// <script type="text/javascript">
+//    (function(){
+//       emailjs.init("EXBJtKBHRblraxYsv");
+//    })();
+// </script>
+
+
+function contact(event) {
+ event.preventDefault();
+ const loading = document.querySelector('.modal__overlay--loading')
+ const success = document.querySelector('.modal__overlay--success')
+ loading.classList += " modal__overlay--visible"
+
+ emailjs
+  .sendForm(
+     'service_gizjigr',
+     'template_j4y6w6p',
+     event.target,
+     'EXBJtKBHRblraxYsv'
+  ).then(() => {
+   loading.classList.remove("modal__overlay--visible");
+   success.classList += " modal__overlay--visible";
+  }).catch(() => {
+    loading.classList.remove("modal__overlay--visible");
+    alert(
+    "The email service is temporarily unavailable. Please contact me directly on archerychinadiva@gmail.com"
+    );
+  })
+}
